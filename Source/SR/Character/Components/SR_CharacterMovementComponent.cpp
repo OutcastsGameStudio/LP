c26c8bb9239d17fcb909fbb26f82bfbeb0345f65
Hask0ll <dobarat.eu@gmail.com>
2024-11-02 22:57:39 +0100

    [FIX] Wall run crash

--- Source/SR/Character/Components/SR_CharacterMovementComponent.cpp
+++ Source/SR/Character/Components/SR_CharacterMovementComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "SR_CharacterMovementComponent.h"
 
+#include "CollisionDebugDrawingPublic.h"
+#include "KismetTraceUtils.h"
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/Character.h"
 #include "Logging/StructuredLog.h" 
@@ -30,6 +32,12 @@ void USR_CharacterMovementComponent::BeginPlay()
 void USR_CharacterMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Draw Line Trace for Wallrun
+	FVector Start = GetCharacterOwner()->GetActorLocation();
+	FVector End = Start + GetCharacterOwner()->GetActorForwardVector() * 200.f;
+	End.Y = End.Y + 45.f;
+	DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, 0.1f);
 }
 
 void USR_CharacterMovementComponent::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
@@ -66,8 +74,42 @@ void USR_CharacterMovementComponent::PhysCustom(float deltaTime, int32 Iteration
 	FHitResult Hit(1.f);
 	SafeMoveUpdatedComponent(Delta, UpdatedComponent->GetComponentRotation(), true, Hit);
 
-	if (FMath::IsNearlyZero(Hit.Normal.Z))
+	if (Hit.IsValidBlockingHit())
+    {
+    	if (FMath::IsNearlyZero(Hit.Normal.Z))
+    	{
+    		SetMovementMode(MOVE_Walking, 0);
+    		return;
+    	}
+    }
+	else if (DetectNextWall(Hit))
 	{
 		SetMovementMode(MOVE_Falling);
+		SafeMoveUpdatedComponent(Delta, UpdatedComponent->GetComponentRotation(), true, Hit);
 	}
+}
+
+void USR_CharacterMovementComponent::UpdateWallRunDirection(FHitResult& Hit)
+{
+	//On utilise la rotation entre le vecteur normal du mur précédent et le vecteur normal du hit (nouveau mur) pour calculer la nouvelle direction
+	FQuat Rotation = FQuat::FindBetweenNormals(WallNormal, Hit.Normal);
+	WallNormal = Hit.Normal;
+	WallRunDirection = Rotation.RotateVector(WallRunDirection);
+}
+
+bool USR_CharacterMovementComponent::DetectNextWall(FHitResult& Hit)
+{
+	UCapsuleComponent* Capsule = GetCharacterOwner()->GetCapsuleComponent();
+	FName CollisionProfile = Capsule->GetCollisionProfileName();
+	
+	//On fait un capsule collision avec les infos de la capsule du personnage.
+	//Puis on fait un sweep vers le mur
+	FVector Start = GetCharacterOwner()->GetActorLocation();
+	FVector End = Start - WallNormal * Capsule->GetScaledCapsuleRadius();
+	FCollisionQueryParams Params = FCollisionQueryParams::DefaultQueryParam;
+	Params.AddIgnoredActor(GetCharacterOwner());
+	
+	GetWorld()->SweepSingleByProfile(Hit, Start, End, Capsule->GetComponentQuat(), CollisionProfile, Capsule->GetCollisionShape(), Params);
+
+	return Hit.bBlockingHit;
 }
